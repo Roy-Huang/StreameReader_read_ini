bb68dc6d6deaddb15f607f0bdcdc6cae1ddffe0d
unknown <roy60531@gmail.com>
2018-11-06 18:04:52 +0800

    Base_Fun_OK

---
+++ StreameReader_read_ini/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace StreameReader_read_ini
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        string FilePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ReadFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = openFileDialog.FileName;           
+            }
+            GetValue(FilePath, textBox_Title.Text, textBox_Item.Text);
+        }
+
+        private void GetValue(string filepath, string Title, string Item)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                StreamReader sr = new StreamReader(FilePath, Encoding.UTF8);
+                bool Title_Check = false;
+                //avoid while loops
+                int loop_flag = 0;
+                while (true)
+                {
+                    string sr_buffer = sr.ReadLine();
+                    //skip start at beginning of the string (null)
+                    if (sr_buffer == null || sr_buffer == "")
+                    {
+                        continue;
+                    }
+                    //skip start at beginning of the string (// and #)
+                    if (Regex.Match(sr_buffer, @"^(//|#).*$").Success)
+                    {
+                        continue;
+                    }
+                    //check "[" and "]"
+                    if (Regex.Match(sr_buffer, @"^\[.*\]").Success)
+                    {
+                        //check Title is right
+                        if (Regex.Match(sr_buffer, Title).Success)
+                        {
+                            Title_Check = true;
+                        }
+                    }
+                    if (Title_Check)
+                    {
+                        string[] GetValue_buffer = sr_buffer.Split('=');
+                        if (Regex.Match(GetValue_buffer[0].Trim(), Item).Success)
+                        {
+                            sb.Append(GetValue_buffer[1].Trim());
+                            break;
+                        }
+                        loop_flag += 1;
+                        if (loop_flag >= 5)
+                        {
+                            MessageBox.Show("Break");
+                            break;
+                        }
+                    }
+                }
+                sr.Close();
+
+                if (CheckIsDigitsOnly(sb.ToString()))
+                {
+                    label_Result.Text = sb.ToString();
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    label_Result.Text = "";
+                    MessageBox.Show("Not Digits, Please Check your ini File");
+                    Console.WriteLine("not");
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+            
+        
+
+        bool CheckIsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
+            return true;
+        }
+
+    }
+}
